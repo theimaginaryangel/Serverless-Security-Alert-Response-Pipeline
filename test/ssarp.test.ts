496ba367145ff62fb5c9b2647e1eca1b379907f8
theimaginaryangel <95882827+theimaginaryangel@users.noreply.github.com>
2026-08-27 16:50:46 +0000

    Initial commit

---
+++ test/ssarp.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Ssarp from '../lib/ssarp-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ssarp-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Ssarp.SsarpStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
